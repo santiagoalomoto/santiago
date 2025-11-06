e2882587a5b5acfbab2e0b07f42e45f478f130d6
Santiago Alomoto <sic.alomoto@yavirac.edu.ec>
2025-11-06 18:29:47 -0500

    cambios1

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("🚀 Hola, soy Santiago Alomoto y este es mi proyecto Python con Docker y CI/CD en GitHub Actions!")
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "🚀 Hola desde el proyecto de Alomoto con Docker y CI/CD!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
